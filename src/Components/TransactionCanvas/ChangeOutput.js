cf221c51d0a2017d18d4db68082d6c14141121e5
misha <tuskar.israel@gmail.com>
2024-09-15 00:08:12 -0500

    Display expected inputs, outputs for  change, and fees

--- src/Components/TransactionCanvas/ChangeOutput.js
+++ src/Components/TransactionCanvas/ChangeOutput.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ChangeOutput = ({ changeOutput, title }) => {
   return (
-    <div className="bg-zinc-400 rounded-lg shadow-md mb-4 mt-4 relative border-2 text-center border-red-800 w-60">
-      <h2 className="text-red-700 font-bold text-lg">
+    <div className="bg-orange-600 rounded-lg shadow-md mb-4 mt-4 relative border-2 text-center border-zinc-300 w-60">
+      <h2 className="text-zinc-300 font-bold text-lg">
         {title}
       </h2>
-      <p className="text-red-700 font-bold text-sm">
+      <p className="text-zinc-300 font-bold text-sm">
         {changeOutput} sats
       </p>
     </div>
